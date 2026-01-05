abea54c2d129bc84307bfbedbea3636dc56705f3
dnmagege <96315904+dnmagege@users.noreply.github.com>
2026-01-05 10:54:50 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <!-- <title>Cover Template · Bootstrap v5.0</title> -->
+    <title>Luxury Wheels Rentals</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+    <!--Css -->
+    <link rel="stylesheet" href="assets/css/cover.css" type="text/css">
+    <!--Fonts -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+
+    
+    <!-- Custom styles for this template -->
+    <!-- <link href="cover.css" rel="stylesheet">
+    <link href="" rel="stylesheet"> -->
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+
+  <!-- add loop to sound or video to repeat -->
+  
+    <video autoplay id="myVideo">
+      <!-- <source src="assets/videos/car (1).mp4" type="video/mp4"> -->
+      <source src="assets/videos/car (3).mp4" type="video/mp4">
+    </video>
+
+    <!-- Car sound -->
+      <iframe src="assets/sounds/sounds.mp3" allow="autoplay" style="display:none" id="iframeAudio"></iframe> 
+
+      <audio autoplay id="playAudio">
+          <source src="assets/sounds/sound.mp3">
+      </audio>
+
+    <div class="container"><a href="home.php" class="home-link btn btn primary"><h4>Home</h4></a><div>
+  
+    <form>
+      <!-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+      <div class="logo large"> <a href="home.php"><img src="assets/images/logo.png" alt="image"/></a> </div>
+
+      <div class="container grey-welcome ">
+        <h1>Luxury Wheels</h1>
+        <h4>Elevate your special day with Luxury Wheel Cars – your gateway to unparalleled luxury on wheels. 
+          Indulge in our exclusive collection of exquisite wedding cars, meticulously crafted to turn every moment into an everlasting memory. 
+          Click  <i><a href="home.php" class="home-linl btn btn primary"><strong >'Home'</strong> </a></i>  to discover our stunning fleet and let us make your wedding journey an extraordinary celebration."</p>
+        <h4>     
+      </div>
+    </form>
+
+    
+  </body>
+</html>
